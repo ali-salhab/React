895f6d3dd3feb7355a10382170269790e7b3c431
Ali salhab <alisalhab258@gmail.com>
2025-10-21 22:20:35 +0300

    install tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,16 @@ import { Route, Routes } from "react-router";
 import HomePags from "./pages/HomePags.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailsPage from "./pages/NoteDetailsPage.jsx";
+import toast from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div>
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <button onClick={() => toast.success("siuccccccccccccc")}>
+        notification
+      </button>
       <Routes>
         <Route path="/" element={<HomePags />}></Route>
         <Route path="/create" element={<CreatePage />}></Route>
