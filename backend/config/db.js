24ab480662a3250990875de7c1ea8367767c62ce
Ali salhab <alisalhab258@gmail.com>
2025-10-21 14:05:55 +0300

    add dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   console.log("Connecting to MongoDB...");
   try {
-    await mongoose.connect(
-      "mongodb+srv://alysalhab5_db_user:<db_password>@cluster0.kre45bt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully👌");
   } catch (error) {
     console.error("MongoDB connection failed:😶‍🌫️🙄", error.message);
+    process.exit(1);
   }
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "../config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 connectDB();
 app.use("/api/notes", notesRoutes);
+console.log(process.env.PORT);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
