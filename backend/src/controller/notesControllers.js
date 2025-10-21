a144d98e0b0f5e37f7318a99eac2a975f5b725e2
Ali salhab <alisalhab258@gmail.com>
2025-10-21 19:11:25 +0300

    controller done

--- backend/src/controller/notesControllers.js
+++ backend/src/controller/notesControllers.js
@@ -1,13 +1,62 @@
-import express from "express";
-export function getAllNotes(req, res) {
-  res.status(200).send("This is some data from the backend!");
+import Note from "../../models/Notes.js";
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find({});
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error fetching notes:", error);
+
+    res.status(500).json({ message: "Server Error" });
+  }
 }
-export function createNote(req, res) {
-  res.status(201).json({ message: "Data created successfully" });
+export async function createNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const newNote = new Note({ title, content });
+    const savedNote = await newNote.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.error("Error creating note:", error);
+
+    res.status(500).json({ message: "Server Error" });
+  }
 }
-export function updateNote(req, res) {
-  res.status(200).json({ message: "Data updated successfully" });
+export async function updateNote(req, res) {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const updatedNote = await Note.findByIdAndUpdate(
+      id,
+      { title, content },
+      { new: true }
+    );
+
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(200).json({
+      message: "Note updated successfully",
+      note: updatedNote,
+    });
+  } catch (error) {
+    console.error("Error updating note:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
 }
-export function deleteNote(req, res) {
-  res.status(200).json({ message: "Data deleted successfully" });
+
+export async function deleteNote(req, res) {
+  const id = req.params.id;
+  try {
+    const deletedNote = await Note.findByIdAndDelete(id);
+
+    if (!deletedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting note:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,18 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "../config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json()); // <-- parses JSON request bodies
+app.use(express.urlencoded({ extended: true }));
 connectDB();
 app.use("/api/notes", notesRoutes);
 console.log(process.env.PORT);
 
+// middleware to parse JSON bodies
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("server start at port 😏😏😏😏😏😉" + PORT);
